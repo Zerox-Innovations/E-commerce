046f17bf2fcb53344bbda82b247b83ff6140e22b
ahdsalman <ahammedsalmanp3@gmail.com>
2024-09-02 17:50:00 +0530

    Swagger Updated Second time

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'account.Account'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
